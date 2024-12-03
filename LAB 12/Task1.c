b6eb15d07efb0c9002d32a320f49e00d8a0d30f4
TEAROFJOY5 <turab653@gmail.com>
2024-12-03 16:34:40 +0500

    Create Task1.c

---
+++ LAB 12/Task1.c
@@ -0,0 +1,19 @@
+/* Task 1: Write a program that calculates the sum of all the elements in array using pointers
+(Note: Generate the array via user)*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{   int a,i,sum,*ptr;
+    printf("Please enter the number of elements in the array: ");
+    scanf("%d",&a);
+    ptr=(int *)calloc(a,sizeof(int));
+    for(i=0; i<a;i++){
+        printf("\nEnter the %d element of the array: ",i+1);
+        scanf("%d",ptr+i);
+        sum=sum+(*(ptr+i));
+     }
+     printf("\nThe sum is %d",sum);
+     free(ptr);
+     
+}
