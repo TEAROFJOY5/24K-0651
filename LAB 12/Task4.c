0eb1f571454d2929e9b1d9d51ac9b75e4b5cfbdc
TEAROFJOY5 <turab653@gmail.com>
2025-05-06 21:43:28 +0500

    Update Task4.c

--- LAB 12/Task4.c
+++ LAB 12/Task4.c
@@ -4,25 +4,32 @@ that 'N' no of characters using malloc or calloc function. Finally copy your ful
 already been taken from the user before
 Dynamic Array = "Muhib Ahmed";*/
 
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{   int num,i;
-    printf("Enter the number of characters in your name(First+Last Name): ");
-    scanf("%d",&num);
     
-    getchar();
-    
-    char *ptr=(char *)calloc(num+1,sizeof(char));
-     
-     if(ptr==NULL)
-     {
-         printf("Memory Allocation Failed!");
-         return 1;
-     }
-    printf("\nEnter your Full Name: ");
+    #include <stdio.h>
+#include <stdlib.h>
 
-      fgets(ptr,num+1,stdin);  
-     printf("Your full name is: %s\n", ptr);
-     free(ptr);
+int main() {
+    int n;
+
+    printf("How many characters are in your full name (without counting spaces)? ");
+    scanf("%d", &n);
+
+    getchar(); // to consume the leftover newline character
+
+    char *name = (char *)malloc((n + 1) * sizeof(char)); // +1 for null terminator
+
+    if (name == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter your full name: ");
+    fgets(name, n + 1, stdin); // read up to n characters
+
+    printf("Your name is: %s\n", name);
+
+    free(name); // free the memory
+    return 0;
 }
+
+    
